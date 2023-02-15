31f09e73d32a6ba5b2c021bfdaf0aa8468ce3118
Chandan Singh <chandan_singh@berkeley.edu>
2023-02-15 13:22:45 -0800

    add eda promptsource

--- scripts/01_fit.py
+++ scripts/01_fit.py
@@ -6,7 +6,7 @@ repo_dir = dirname(dirname(os.path.abspath(__file__)))
 # List of values to sweep over (sweeps over all combinations of these)
 params_shared_dict = {
     'seed': [1],
-    'save_dir': [join('/home/chansingh/mntv1', 'tree-prompt', 'feb15')],
+    'save_dir': [join('/home/chansingh/mntv1', 'tree-prompt', 'feb16')],
     'use_cache': [1], # pass binary values with 0/1 instead of the ambiguous strings True/False
     # 'dataset_name': ['rotten_tomatoes', 'financial_phrasebank', 'emotion', 'sst2'],
     'dataset_name': ['rotten_tomatoes'], #, 'financial_phrasebank', 'emotion', 'sst2'],
@@ -16,8 +16,8 @@ params_shared_dict = {
 
 # List of tuples to sweep over (these values are coupled, and swept over together)
 params_coupled_dict = {
-    ('model_name', 'checkpoint'): [
-        ('manual_tree', 'gpt2-xl')
+    ('model_name', 'checkpoint', 'batch_size'): [
+        ('manual_tree', 'gpt2-xl', 32)
     ],
     # ('model_name', 'split_strategy'): [
     #     ('tprompt', 'iprompt'),
@@ -34,6 +34,6 @@ submit_utils.run_args_list(
     args_list,
     script_name=join(repo_dir, 'experiments', '01_fit.py'),
     actually_run=True,
-    gpu_ids = [2],
+    gpu_ids = [1, 2],
     shuffle=False,
 )
